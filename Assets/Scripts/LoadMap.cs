8f2c8090e7e3eac839fdf6350bdb5d5f0b570fc2
Guangzheng <2450726169@qq.com>
2024-09-29 02:39:12 +1000

    PacStudent Movement

--- Assets/Scripts/LoadMap.cs
+++ Assets/Scripts/LoadMap.cs
@@ -6,122 +6,140 @@ using UnityEngine.UIElements;
 public class LoadMap : MonoBehaviour
 {
     [SerializeField] private GameObject[] sprite;
+    [SerializeField] private GameObject[] Ghosts;
+    [SerializeField] private Transform PacStudent;
+    [SerializeField] private AudioSource backgroundAudio;
+    [SerializeField] private AudioSource PacStudentAudio;
+    [SerializeField] private AudioClip[] backgroundAudioClips;
+    [SerializeField] private AudioClip[] PacStudentAudioClips;
+    public Animator PacStudentController;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float startTime;
+    private float speed;
+    int[,] map;
+    int row, col;
+    int direction;
+    int x, y;
+
+    int[,] levelMap =
+        {
+            {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+            {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+            {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+            {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+            {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+            {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+            {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+            {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+            {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+            {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+            {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+        };
 
     // Start is called before the first frame update
     void Start()
     {
-        int[,] levelMap1 =
-         {
-         {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
-         {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
-         {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
-         {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
-         {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
-         {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
-         {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
-         {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
-         {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
-         {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
-         {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
-         {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
-         {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
-         {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
-         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
-         };
-        int row = levelMap1.GetLength(0), col = levelMap1.GetLength(1);
         int i, j;
-        int[,] levelMap = new int[2 * row - 1, 2 * col];
+        Time.timeScale = 0;
+        row = levelMap.GetLength(0);
+        col = levelMap.GetLength(1);
+        map = new int[2 * row - 1, 2 * col];
         for (i = 0; i < 2 * row - 1; i++)
             for (j = 0; j < 2 * col; j++)
                 if (i < row)
                     if (j < col)
-                        levelMap[i, j] = levelMap1[i, j];
+                        map[i, j] = levelMap[i, j];
                     else
-                        levelMap[i, j] = levelMap[i, 2 * col - j - 1];
+                        map[i, j] = map[i, 2 * col - j - 1];
                 else
                     if (j < col)
-                        levelMap[i, j] = levelMap[2 * row - i - 2, j];
+                        map[i, j] = map[2 * row - i - 2, j];
                     else
-                        levelMap[i, j] = levelMap[2 * row - i - 2, 2 * col - j - 1];
+                        map[i, j] = map[2 * row - i - 2, 2 * col - j - 1];
         row += row - 1;
         col += col;
         for (i = 0; i < row; i++)
             for (j = 0; j < col; j++)
-                switch (levelMap[i,j])
+                switch (map[i,j])
                 {
                     case 0:
                         break;
                     case 1:
-                        if (i + 1 < row && (levelMap[i + 1, j] == 1 || levelMap[i + 1, j] == 2))
+                        if (i + 1 < row && (map[i + 1, j] == 1 || map[i + 1, j] == 2))
                         {
-                            if (j + 1 < col && (levelMap[i, j + 1] == 1 || levelMap[i, j + 1] == 2))
+                            if (j + 1 < col && (map[i, j + 1] == 1 || map[i, j + 1] == 2))
                                 GameObject.Instantiate(sprite[0], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
-                            else if (j > 0 && (levelMap[i, j - 1] == 1 || levelMap[i, j - 1] == 2))
+                            else if (j > 0 && (map[i, j - 1] == 1 || map[i, j - 1] == 2))
                                 GameObject.Instantiate(sprite[0], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, -90.0f));
                         }
-                        else if (i > 0 && (levelMap[i - 1, j] == 1 || levelMap[i - 1, j] == 2))
+                        else if (i > 0 && (map[i - 1, j] == 1 || map[i - 1, j] == 2))
                         {
-                            if (j + 1 < col && (levelMap[i, j + 1] == 1 || levelMap[i, j + 1] == 2))
+                            if (j + 1 < col && (map[i, j + 1] == 1 || map[i, j + 1] == 2))
                                 GameObject.Instantiate(sprite[0], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 90.0f));
-                            else if (j > 0 && (levelMap[i, j - 1] == 1 || levelMap[i, j - 1] == 2))
+                            else if (j > 0 && (map[i, j - 1] == 1 || map[i, j - 1] == 2))
                                 GameObject.Instantiate(sprite[0], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 180.0f));
                         }
                         break;
                     case 2:
-                        if (i > 0 && (levelMap[i - 1,j] == 1 || levelMap[i - 1, j] == 2) && i + 1 < row && (levelMap[i + 1, j] == 1 || levelMap[i + 1, j] == 2))
+                        if (i > 0 && (map[i - 1,j] == 1 || map[i - 1, j] == 2) && i + 1 < row && (map[i + 1, j] == 1 || map[i + 1, j] == 2))
                             GameObject.Instantiate(sprite[1], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
                         else
                             GameObject.Instantiate(sprite[1], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 90.0f));
                         break;
                     case 3:
                         bool flag = false;
-                        if (levelMap[i + 1, j] == 3 || levelMap[i + 1, j] == 4 || levelMap[i + 1, j] == 7)
+                        if (map[i + 1, j] == 3 || map[i + 1, j] == 4 || map[i + 1, j] == 7)
                         {
-                            if (levelMap[i, j + 1] == 3 || levelMap[i, j + 1] == 4 || levelMap[i, j + 1] == 7)
+                            if (map[i, j + 1] == 3 || map[i, j + 1] == 4 || map[i, j + 1] == 7)
                             {
-                                if (levelMap[i - 1, j] == 3 || levelMap[i - 1, j] == 4 || levelMap[i - 1, j] == 7 || levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4 || levelMap[i, j - 1] == 7)
+                                if (map[i - 1, j] == 3 || map[i - 1, j] == 4 || map[i - 1, j] == 7 || map[i, j - 1] == 3 || map[i, j - 1] == 4 || map[i, j - 1] == 7)
                                     flag = true;
                                 else
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
                             }
-                            else if (levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4 || levelMap[i, j - 1] == 7)
+                            else if (map[i, j - 1] == 3 || map[i, j - 1] == 4 || map[i, j - 1] == 7)
                             {
-                                if (levelMap[i - 1, j] == 3 || levelMap[i - 1, j] == 4 || levelMap[i - 1, j] == 7)
+                                if (map[i - 1, j] == 3 || map[i - 1, j] == 4 || map[i - 1, j] == 7)
                                     flag = true;
                                 else
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, -90.0f));
                             }
                         }
-                        else if (levelMap[i - 1, j] == 3 || levelMap[i - 1, j] == 4 || levelMap[i - 1, j] == 7)
+                        else if (map[i - 1, j] == 3 || map[i - 1, j] == 4 || map[i - 1, j] == 7)
                         {
-                            if (levelMap[i, j + 1] == 3 || levelMap[i, j + 1] == 4 || levelMap[i, j + 1] == 7)
+                            if (map[i, j + 1] == 3 || map[i, j + 1] == 4 || map[i, j + 1] == 7)
                             {
-                                if (levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4 || levelMap[i, j - 1] == 7)
+                                if (map[i, j - 1] == 3 || map[i, j - 1] == 4 || map[i, j - 1] == 7)
                                     flag = true;
                                 else
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 90.0f));
                             }
-                            else if (levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4 || levelMap[i, j - 1] == 7)
+                            else if (map[i, j - 1] == 3 || map[i, j - 1] == 4 || map[i, j - 1] == 7)
                                 GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 180.0f));
                         }
                         if (flag)
-                            if (levelMap[i + 1, j] == 3 && levelMap[i + 2, j] != 3 && levelMap[i + 2, j] != 4 || levelMap[i + 1, j] == 4 && (levelMap[i + 2, j] == 3 || levelMap[i + 2, j] == 4) || levelMap[i + 1, j] == 7)
+                            if (map[i + 1, j] == 3 && map[i + 2, j] != 3 && map[i + 2, j] != 4 || map[i + 1, j] == 4 && (map[i + 2, j] == 3 || map[i + 2, j] == 4) || map[i + 1, j] == 7)
                             {
-                                if (levelMap[i, j + 1] == 3 && levelMap[i, j + 2] != 3 && levelMap[i, j + 2] != 4 || levelMap[i, j + 1] == 4 && (levelMap[i, j + 2] == 3 || levelMap[i, j + 2] == 4) || levelMap[i, j + 1] == 7)
+                                if (map[i, j + 1] == 3 && map[i, j + 2] != 3 && map[i, j + 2] != 4 || map[i, j + 1] == 4 && (map[i, j + 2] == 3 || map[i, j + 2] == 4) || map[i, j + 1] == 7)
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
-                                else if (levelMap[i, j - 1] == 3 && levelMap[i, j - 2] != 3 && levelMap[i, j - 2] != 4 || levelMap[i, j - 1] == 4 && (levelMap[i, j - 2] == 3 || levelMap[i, j - 2] == 4) || levelMap[i, j - 1] == 7)
+                                else if (map[i, j - 1] == 3 && map[i, j - 2] != 3 && map[i, j - 2] != 4 || map[i, j - 1] == 4 && (map[i, j - 2] == 3 || map[i, j - 2] == 4) || map[i, j - 1] == 7)
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, -90.0f));
                             }
-                            else if (levelMap[i - 1, j] == 3 && levelMap[i - 2, j] != 3 && levelMap[i - 2, j] != 4 || levelMap[i - 1, j] == 4 && (levelMap[i - 2, j] == 3 || levelMap[i - 2, j] == 4) || levelMap[i - 1, j] == 7)
+                            else if (map[i - 1, j] == 3 && map[i - 2, j] != 3 && map[i - 2, j] != 4 || map[i - 1, j] == 4 && (map[i - 2, j] == 3 || map[i - 2, j] == 4) || map[i - 1, j] == 7)
                             {
-                                if (levelMap[i, j + 1] == 3 && levelMap[i, j + 2] != 3 && levelMap[i, j + 2] != 4 || levelMap[i, j + 1] == 4 && (levelMap[i, j + 2] == 3 || levelMap[i, j + 2] == 4) || levelMap[i, j + 1] == 7)
+                                if (map[i, j + 1] == 3 && map[i, j + 2] != 3 && map[i, j + 2] != 4 || map[i, j + 1] == 4 && (map[i, j + 2] == 3 || map[i, j + 2] == 4) || map[i, j + 1] == 7)
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 90.0f));
-                                else if (levelMap[i, j - 1] == 3 && levelMap[i, j - 2] != 3 && levelMap[i, j - 2] != 4 || levelMap[i, j - 1] == 4 && (levelMap[i, j - 2] == 3 || levelMap[i, j - 2] == 4) || levelMap[i, j - 1] == 7)
+                                else if (map[i, j - 1] == 3 && map[i, j - 2] != 3 && map[i, j - 2] != 4 || map[i, j - 1] == 4 && (map[i, j - 2] == 3 || map[i, j - 2] == 4) || map[i, j - 1] == 7)
                                     GameObject.Instantiate(sprite[2], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 180.0f));
                             }
                         break;
                     case 4:
-                        if ((levelMap[i - 1, j] == 3 || levelMap[i - 1, j] == 4 || levelMap[i - 1, j] == 7) && (levelMap[i + 1, j] == 3 || levelMap[i + 1, j] == 4 || levelMap[i + 1, j] == 7))
+                        if ((map[i - 1, j] == 3 || map[i - 1, j] == 4 || map[i - 1, j] == 7) && (map[i + 1, j] == 3 || map[i + 1, j] == 4 || map[i + 1, j] == 7))
                             GameObject.Instantiate(sprite[3], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
                         else
                             GameObject.Instantiate(sprite[3], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 90.0f));
@@ -133,27 +151,133 @@ public class LoadMap : MonoBehaviour
                         GameObject.Instantiate(sprite[5], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
                         break;
                     case 7:
-                        if (j > 0 && (levelMap[i, j - 1] == 1 || levelMap[i, j - 1] == 2 || levelMap[i, j - 1] == 7) && j + 1 < col && (levelMap[i, j + 1] == 1 || levelMap[i, j + 1] == 2 || levelMap[i, j + 1] == 7))
+                        if (j > 0 && (map[i, j - 1] == 1 || map[i, j - 1] == 2 || map[i, j - 1] == 7) && j + 1 < col && (map[i, j + 1] == 1 || map[i, j + 1] == 2 || map[i, j + 1] == 7))
                         {
-                            if (i + 1 < row && (levelMap[i + 1, j] == 3 || levelMap[i + 1, j] == 4))
+                            if (i + 1 < row && (map[i + 1, j] == 3 || map[i + 1, j] == 4))
                                 GameObject.Instantiate(sprite[6], new Vector3(j - col / 2, row / 2 - i, 0.0f), new Quaternion());
-                            else if (i > 0 && (levelMap[i - 1, j] == 3 || levelMap[i - 1, j] == 4))
+                            else if (i > 0 && (map[i - 1, j] == 3 || map[i - 1, j] == 4))
                                 GameObject.Instantiate(sprite[6], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 180.0f));
                         }
-                        else if (i > 0 && (levelMap[i - 1, j] == 1 || levelMap[i - 1, j] == 2 || levelMap[i - 1, j] == 7) && i + 1 < row && (levelMap[i + 1, j] == 1 || levelMap[i + 1, j] == 2 || levelMap[i + 1, j] == 7))
+                        else if (i > 0 && (map[i - 1, j] == 1 || map[i - 1, j] == 2 || map[i - 1, j] == 7) && i + 1 < row && (map[i + 1, j] == 1 || map[i + 1, j] == 2 || map[i + 1, j] == 7))
                         {
-                            if (j > 0 && (levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4))
+                            if (j > 0 && (map[i, j - 1] == 3 || map[i, j - 1] == 4))
                                 GameObject.Instantiate(sprite[6], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, 90.0f));
-                            else if (j + 1 < col && (levelMap[i, j + 1] == 3 || levelMap[i, j + 1] == 4))
+                            else if (j + 1 < col && (map[i, j + 1] == 3 || map[i, j + 1] == 4))
                                 GameObject.Instantiate(sprite[6], new Vector3(j - col / 2, row / 2 - i, 0.0f), Quaternion.Euler(0, 0, -90.0f));
                         }
                         break;
                 }
+
+        PacStudent.position = new Vector3(1 - col / 2, row / 2 - 1, 0.0f);
+        speed = 1.0f;
+        direction = 0;
+        x = 1;
+        y = 1;
+        studentMove();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!backgroundAudio.isPlaying)
+        {
+            Time.timeScale = 1;
+            backgroundAudio.clip = backgroundAudioClips[1];
+            backgroundAudio.Play();
+        }
+        if (Vector3.Distance(PacStudent.position, endPos) > 0.1f)
+        {
+            PacStudent.position = Vector3.Lerp(startPos, endPos, (Time.time - startTime) / (Vector3.Distance(startPos, endPos) / speed));
+            if (!PacStudentAudio.isPlaying && Time.timeScale != 0)
+            {
+                PacStudentAudio.clip = PacStudentAudioClips[0];
+                PacStudentAudio.Play();
+            }
+        }
+        else
+        {
+            PacStudent.position = endPos;
+            PacStudentController.SetTrigger("endMove");
+            PacStudentAudio.Stop();
+            direction = (direction + 1) % 4;
+            studentMove();
+        }
+    }
+
+    void studentMove()
+    {
+        startPos = PacStudent.position;
+        startTime = Time.time;
+        switch (direction)
+        {
+            case 0:
+                PacStudentController.SetTrigger("right");
+                while (true)
+                {
+                    if (x + 1 == col)
+                        break;
+                    if (map[y, x + 1] == 0 || map[y, x + 1] == 5 || map[y, x + 1] == 6)
+                        x++;
+                    else
+                        break;
+                    if (y + 1 == row)
+                        break;
+                    if (map[y + 1, x] == 0 || map[y + 1, x] == 5 || map[y + 1, x] == 6)
+                        break;
+                }
+                endPos = new Vector3(x - col / 2, row / 2 - y, 0.0f);
+                break;
+            case 1:
+                PacStudentController.SetTrigger("down");
+                while (true)
+                {
+                    if (y + 1 == row)
+                        break;
+                    if (map[y + 1, x] == 0 || map[y + 1, x] == 5 || map[y + 1, x] == 6)
+                        y++;
+                    else
+                        break;
+                    if (x == 0)
+                        break;
+                    if (map[y, x - 1] == 0 || map[y, x - 1] == 5 || map[y, x - 1] == 6)
+                        break;
+                }
+                endPos = new Vector3(x - col / 2, row / 2 - y, 0.0f);
+                break;
+            case 2:
+                PacStudentController.SetTrigger("left");
+                while (true)
+                {
+                    if (x == 0)
+                        break;
+                    if (map[y, x - 1] == 0 || map[y, x - 1] == 5 || map[y, x - 1] == 6)
+                        x--;
+                    else
+                        break;
+                    if (y == 0)
+                        break;
+                    if (map[y - 1, x] == 0 || map[y - 1, x] == 5 || map[y - 1, x] == 6)
+                        break;
+                }
+                endPos = new Vector3(x - col / 2, row / 2 - y, 0.0f);
+                break;
+            case 3:
+                PacStudentController.SetTrigger("up");
+                while (true)
+                {
+                    if (y == 0)
+                        break;
+                    if (map[y - 1, x] == 0 || map[y - 1, x] == 5 || map[y - 1, x] == 6)
+                        y--;
+                    else
+                        break;
+                    if (x + 1 == col)
+                        break;
+                    if (map[y, x + 1] == 0 || map[y, x + 1] == 5 || map[y, x + 1] == 6)
+                        break;
+                }
+                endPos = new Vector3(x - col / 2, row / 2 - y, 0.0f);
+                break;
+        }
     }
 }
